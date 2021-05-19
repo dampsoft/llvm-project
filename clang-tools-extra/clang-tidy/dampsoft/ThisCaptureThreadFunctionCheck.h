813b8abf4cc3a82375adb2e418ee0a3b63c6bc07
Rasmus Thomsen <rasmus.thomsen@dampsoft.de>
2021-05-19 17:24:37 +0200

    Add dampsoft-this-capture-thread-function

---
+++ clang-tools-extra/clang-tidy/dampsoft/ThisCaptureThreadFunctionCheck.h
@@ -0,0 +1,38 @@
+//===--- ThisCaptureThreadFunctionCheck.h - clang-tidy ----------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_DAMPSOFT_THISCAPTURETHREADFUNCTIONCHECK_H
+#define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_DAMPSOFT_THISCAPTURETHREADFUNCTIONCHECK_H
+
+#include "../ClangTidyCheck.h"
+
+namespace clang {
+namespace tidy {
+namespace dampsoft {
+
+/// FIXME: Write a short description.
+///
+/// For the user-facing documentation see:
+/// http://clang.llvm.org/extra/clang-tidy/checks/dampsoft-this-capture-thread-function.html
+class ThisCaptureThreadFunctionCheck : public ClangTidyCheck {
+public:
+  ThisCaptureThreadFunctionCheck(StringRef Name, ClangTidyContext *Context)
+      : ClangTidyCheck(Name, Context) {}
+  void registerMatchers(ast_matchers::MatchFinder *Finder) override;
+  void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
+
+private:
+  void analyseLambda(const CXXConstructExpr *MatchedDecl,
+                     const Expr *LambdaTerm);
+};
+
+} // namespace dampsoft
+} // namespace tidy
+} // namespace clang
+
+#endif // LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_DAMPSOFT_THISCAPTURETHREADFUNCTIONCHECK_H
