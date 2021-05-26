3a0a5e476a482720b12dcb25631d784ac7243046
Rasmus Thomsen <rasmus.thomsen@dampsoft.de>
2021-05-26 16:44:56 +0200

    Add dampsoft-lambda-capture-leak

---
+++ clang-tools-extra/test/clang-tidy/checkers/dampsoft-lambda-capture-leak.cpp
@@ -0,0 +1,20 @@
+// RUN: %check_clang_tidy %s dampsoft-lambda-capture-leak %t
+
+class Test {
+public:
+  template <typename L>
+  void addListener(L lambda) {
+    lambda();
+  }
+
+  void doNothing() const {}
+};
+
+void test() {
+  auto TestInstance = Test();
+  TestInstance.addListener([TestInstance]() { TestInstance.doNothing(); });
+  // CHECK-MESSAGES: :[[@LINE-1]]:16: warning: Lambda leak (self scoped SharedPtr) found [dampsoft-lambda-capture-leak]
+
+  auto TestInstance2 = Test();
+  TestInstance.addListener([TestInstance2]() { TestInstance2.doNothing(); });
+}
